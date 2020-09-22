ff4126c493645f8dac5f8aee2d7f1e9060d76379
Breno Souza <breno.vitoi@gmail.com>
2020-09-22 15:39:13 -0300

    Criado as classes TipoData, Movimentacao, Ocorrencia, Calendario

--- src/main/java/com/daemoncyber/sistemacontrole/model/TipoData.java
+++ src/main/java/com/daemoncyber/sistemacontrole/model/TipoData.java
@@ -2,18 +2,13 @@ package com.daemoncyber.sistemacontrole.model;
 
 import lombok.*;
 
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 @Builder
-public class Localidade {
-    private Long id;
-    @ManyToOne
-    private NivelAcesso nivelAcesso;
+public class TipoData {
+    private long id;
     private String descricao;
 }
